cd990ab869d54bfaf28ed018f063e52e69d28d44
Sri Sudharsan S <srisudharsan432@gmail.com>
2025-09-02 11:51:16 +0530

    first commit

---
+++ src/main/java/com/example/devopsexample/DevopsexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsexampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsexampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/devopsexample/DevopsexampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsexampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
